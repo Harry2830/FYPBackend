50e5abcd05226536d4a93bcfe5f16508a2c5ac92
Harry2830 <m.haris0771@gmail.com>
2025-05-11 02:01:15 +0500

    port changed

--- app.py
+++ app.py
@@ -764,7 +764,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=5000,
+        port=8080,
         reload=True,
         # workers=8
     )
